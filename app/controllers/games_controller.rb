006caced12693d34cc0e0ed7b629685b0e1c3cfe
Alexander Weir <CryptoPynchon@gmail.com>
2018-11-05 16:09:00 -0500

    yeah

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,7 @@
 class GamesController < ApplicationController
   def new
     # TODO: generate random grid of letter
+    @start_time = Time.now.strftime('%s').to_i
     alphabet = ("A".."Z").to_a
     @grid = []
     counter = 0
@@ -11,48 +12,47 @@ class GamesController < ApplicationController
     @grid # this is an array
   end
 
+  def included?(attempt, grid)
+    attempt.chars.all? { |letter| attempt.count(letter) <= grid.count(letter) }
+  end
+
+  def compute_score(attempt, time_taken)
+    time_taken > 60.0 ? 0 : attempt.size * (1.0 - time_taken / 60.0)
+  end
+
+  def english_word?(word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
+  end
+
+  def score_and_message(attempt, grid, time)
+    if included?(attempt.upcase, grid)
+      if english_word?(attempt)
+        score = compute_score(attempt, time)
+        [score, "well done"]
+      else
+        [0, "not an english word"]
+      end
+    else
+      [0, "not in the grid"]
+    end
+  end
+
+  def run_game(attempt, grid, start_time, end_time)
+    @result = { time: end_time.to_i - start_time.to_i }
+    score_and_message = score_and_message(attempt, grid, @result[:time])
+    @result[:score] = score_and_message.first
+    @result[:message] = score_and_message.last
+
+    @result
+  end
+
   def score
-    raise
+    @word = params[:attempt]
+    @grid = params[:grid]
+    @start_time = params[:start_time]
+    @end_time = Time.now.strftime('%s').to_i
+    run_game(@word, @grid, @start_time, @end_time)
   end
 end
-
-# def run_game(attempt, grid, start_time, end_time)
-#   # TODO: runs the game and return detailed hash of result
-#   relevant_data = ""
-#   open("https://wagon-dictionary.herokuapp.com/#{attempt}", "r") do |file|
-#     file.each_line do |line|
-#       relevant_data = line.split('\n')[0]
-#     end
-#   end
-
-#   p relevant_data.class
-
-#   attempt = attempt.upcase
-
-#   result = JSON.parse(relevant_data)
-
-#   p result.class
-
-#   compare_attempt_array = attempt.split("")
-
-#   p compare_attempt_array.class
-
-#   if compare_attempt_array.all? { |ltr| grid.include?(ltr) } == false
-#     result[:score] = 0
-#     result[:message] = "#{attempt} is not in the grid"
-#   elsif result["found"] == false
-#     result[:score] = 0
-#     result[:message] = "#{attempt} is not an english word"
-#   else
-#     result[:time] = (end_time.sec.to_i - start_time.sec.to_i)
-#     result[:score] = result["length"] - result[:time]
-#     result[:message] = "well done"
-#   end
-
-#   result
-# end
-# # %w{ant bear cat}.all? {|word| word.length >= 3}
-# # run_game(apple, )
-
-# p run_game("YXK", ["Y", "X", "K", "Z", "B", "I", "T", "C", "H"], Time.now, Time.now + 1)
-#   end
